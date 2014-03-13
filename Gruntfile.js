ca1a15162b16d0daf7d80d0f8654556d6a1a8aad
David Koblas <david@tubularlabs.com>
2014-03-13 10:25:46 -0700

    more cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,37 @@ module.exports = function(grunt) {
     browserify: {
       js: {
         // A single entry point for our app
-        src: 'private/app/js/app.js',
+        src: 'fe/app/app.js',
         // Compile to a single file to add a script tag for in your HTML
         dest: 'static/app/app.js',
       },
+    },
+    watch: {
+      js: {
+          files: 'fe/**',
+          tasks: ['browserify', 'copy'],
+          options: {
+              spawn: false
+          }
+      }
     },
     copy: {
       all: {
         // This copies all the html and css into the dist/ folder
         expand: true,
-        cwd: 'app/',
-        src: ['**/*.html', '**/*.css'],
-        dest: 'dist/',
+        cwd: 'fe/',
+        src: ['**/*.html', '**/*.css', 'vendor/**', 'bootstrap/**'],
+        dest: 'static/',
       },
     },
   });
 
   // Load the npm installed tasks
   grunt.loadNpmTasks('grunt-browserify');
+  // grunt.loadNpmTasks('grunt-watchify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // The default tasks to run when you type: grunt
-  grunt.registerTask('default', ['browserify', 'copy']);
+  grunt.registerTask('default', ['browserify', 'copy', 'watch']);
 };
